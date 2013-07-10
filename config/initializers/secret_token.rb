78dfe444c23cbb8bf63e61fe9cbed82990888741
Tomas Romero <tomas@tomas.internal.zauberlabs.com>
2013-07-10 17:06:42 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeoSf72::Application.config.secret_token = 'a3b980e6080375f83c1a7c42490ed5f6af027e1699a1dceb9b359aaccd826ce2f4d46ef8a35eb476ff9b02d54ae1a0020ac8effb0ca528c5512b29bebfdaa47b'
