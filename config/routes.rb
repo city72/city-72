817ac4c874519f1a613bd2d2765b20c5560a2ef8
Tomas Romero <tomas@tomas.internal.zauberlabs.com>
2013-07-12 12:01:24 -0300

    post-rebase fix

--- config/routes.rb
+++ config/routes.rb
@@ -10,17 +10,17 @@ IdeoSf72::Application.routes.draw do
 
   root to: 'home#home'
 
-  get "prepare" => "home_controller#prepare"
+  get "prepare" => "home#prepare"
   
   get "home" => "home#home"
 
   get "connect" => "home#connect"
 
-  get "stories" => "home_controller#stories"
+  get "stories" => "home#stories"
 
-  get "things" => "home_controller#things"
+  get "things" => "home#things"
 
-  get "story_detail" => "home_controller#story_detail"
+  get "story_detail" => "home#story_detail"
 
   get "about" => "home#about"
   
