35366a29737610b1466a8f4e5f6df7d7c12bc045
Tomas Romero <tomas@tomas.internal.zauberlabs.com>
2013-07-17 19:40:07 -0300

    item abm

---
+++ app/admin/items.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register Item do
+  
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ActiveRecord::Base
   attr_accessible :category, :image, :title, :use_case
+
+  mount_uploader :image, ImageUploader
+
+  validates :category, inclusion: { in: %w(Essential Useful Personal),
+    message: "%{value} is not a valid category" }
 end
