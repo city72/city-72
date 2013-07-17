744d87453c2eff25180cfa9f63cbe1ccbfe16454
Tomas Romero <tomas@tomas.internal.zauberlabs.com>
2013-07-17 19:20:00 -0300

    disable postgis on map_marker model

--- app/models/map_marker.rb
+++ app/models/map_marker.rb
@@ -2,8 +2,8 @@ class MapMarker < ActiveRecord::Base
   attr_accessible :lon_lat, :message
 
   # By default, use the GEOS implementation for spatial columns.
-  self.rgeo_factory_generator = RGeo::Geos.factory_generator
+  # self.rgeo_factory_generator = RGeo::Geos.factory_generator
 
   # But use a geographic implementation for the :lonlat column.
-  set_rgeo_factory_for_column(:lon_lat, RGeo::Geographic.spherical_factory(:srid => 4326))
+  # set_rgeo_factory_for_column(:lon_lat, RGeo::Geographic.spherical_factory(:srid => 4326))
 end
