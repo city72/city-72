ddceddb1a57bdd09fe53aa2912e82cc7b9e1a7c3
Mariano A. Cortesi <mariano@zauberlabs.com>
2013-07-18 04:13:41 -0300

    beautify active admin for items & kits

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register Item do
-  
+  config.paginate = false
+
+  scope :all
+  [:Essential, :Useful, :Personal].each do |tag|
+    scope tag do |items|
+      items.where("category = '#{tag}'")
+    end
+  end
+
+  # hide the links entirely
+  index :download_links => false do
+    selectable_column
+    column :title
+    column :image do |item|
+      image_tag(item.image, size: "100x100")
+    end
+    column :category
+    default_actions
+  end
+
+
 end
--- app/admin/kits.rb
+++ app/admin/kits.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Kit do
-  
+  config.paginate = false
+
+  index do
+    selectable_column
+    column :title
+    column :image do |kit|
+      image_tag(kit.image, size: "100x100")
+    end
+    default_actions
+  end
 end
