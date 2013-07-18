3550e90511693886ab20eec844d082cb8c5900dd
Tomas Romero <tomas@tomas.internal.zauberlabs.com>
2013-07-18 18:50:18 -0300

    update stories admin layout

--- app/admin/stories.rb
+++ app/admin/stories.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register Story do
-  
+	config.paginate = false
+
+	index do
+		selectable_column
+		column :main_image_url do |story|
+			if story.main_image_url.to_s != ""
+				image_tag(story.main_image_url_url, size: "100x100")
+			end
+		end
+		column :person
+		column :event
+		column :location
+		default_actions
+	end
 end
