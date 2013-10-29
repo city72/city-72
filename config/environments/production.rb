32e592e255accfb1501fa30e90034b2de041666c
Tomas Romero <tomas@tomas.internal.zauberlabs.com>
2013-10-29 12:31:12 -0300

    adding joyride to asset precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ IdeoSf72::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += ['*.js',
                                 #add css not imported in application.css.scss
-                                'joysadfsdfsdride-2.1.css', 'active_admin.css', 'admin_no_cms.css', 'jquery.modal.css',
+                                'joyride-2.1.css', 'active_admin.css', 'admin_no_cms.css', 'jquery.modal.css',
                                 '*.png', '*.jpg', '*.jpeg', '*.gif']
 
   client = Dalli::Client.new
