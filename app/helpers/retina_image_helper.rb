e6a71d1722d68af6717950a1b2013d6dcb8e8c2d
damian <damian@zauberlabs.com>
2014-04-28 16:33:36 -0300

    adding alts to retina images

--- app/helpers/retina_image_helper.rb
+++ app/helpers/retina_image_helper.rb
@@ -1,10 +1,10 @@
 module RetinaImageHelper
 
-  def retina_image_tag image
-    "<img class=\"retina-img\" alt=\":) \" data-retina-url=\"#{image.retina.url}\" src=\"#{image.common.url}\" width=\"300\" height=\"190\" />".html_safe
+  def retina_image_tag image, title
+    "<img class=\"retina-img\" alt=\"#{title} \" data-retina-url=\"#{image.retina.url}\" src=\"#{image.common.url}\" width=\"300\" height=\"190\" />".html_safe
   end
 
-  def full_bleed_image_tag image
-    "<img class=\"retina-img\" alt=\":) \" data-retina-url=\"#{image.retina_full_bleed.url}\" src=\"#{image.common_full_bleed.url}\" width=\"465\" height=\"190\" />".html_safe
+  def full_bleed_image_tag image, title
+    "<img class=\"retina-img\" alt=\"#{title} \" data-retina-url=\"#{image.retina_full_bleed.url}\" src=\"#{image.common_full_bleed.url}\" width=\"465\" height=\"190\" />".html_safe
   end
 end
