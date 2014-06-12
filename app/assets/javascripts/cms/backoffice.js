0be1526992c344f48c353512a42233894eec0af0
Paul Ballesty <paulballesty@pauls-mbp.internal.zauberlabs.com>
2014-06-12 12:05:29 -0300

    Buttons feedback using ladda-buttons

--- app/assets/javascripts/cms/backoffice.js
+++ app/assets/javascripts/cms/backoffice.js
@@ -2,11 +2,14 @@
 //= require underscore-min.js
 //= require angular/angular
 //= require angular-resource/angular-resource
+//= require ladda-bootstrap/dist/spin
+//= require ladda-bootstrap/dist/ladda
+//= require ./ui-ladda
 //= require_self
 //= require_tree ./services
 //= require_tree ./controllers
 
-var backofficeApp = angular.module('backofficeApp', ['ngResource']);
+var backofficeApp = angular.module('backofficeApp', ['ngResource', 'ui.ladda']);
 
 backofficeApp.config(["$httpProvider", function($httpProvider) {
   $httpProvider.defaults.headers.common['X-CSRF-Token'] = angular.element('meta[name=csrf-token]').attr('content');
