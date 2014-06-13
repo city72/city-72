475528d4d884da7dd854b8afe86638f96b567ef6
Paul Ballesty <paulballesty@pauls-mbp.internal.zauberlabs.com>
2014-06-13 15:37:21 -0300

    Provisional fix in asset pipeline config to deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require jquery/dist/jquery
 //= require jquery_ujs
 //= require underscore/underscore
 //= require modernizr/modernizr
