3f5f6009214d3a7201d4d5cfd76d979c5dd7da6a
Paul Ballesty <paulballesty@pauls-mbp.internal.zauberlabs.com>
2014-06-16 15:26:31 -0300

    Adding included field to city network model

--- app/models/city_network.rb
+++ app/models/city_network.rb
@@ -1,3 +1,3 @@
 class CityNetwork < ActiveRecord::Base
-  attr_accessible :headline, :in_an_emergency, :now, :network_url, :fixed
+  attr_accessible :headline, :in_an_emergency, :now, :network_url, :included, :fixed
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140616175848) do
+ActiveRecord::Schema.define(:version => 20140616181532) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(:version => 20140616175848) do
     t.datetime "updated_at",      :null => false
     t.boolean  "fixed"
     t.string   "network_url"
+    t.boolean  "included"
   end
 
   create_table "current_modes", :force => true do |t|
