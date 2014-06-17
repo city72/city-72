789f157be0afd9a55863dc48015a670d3650a4c7
Paul Ballesty <paulballesty@pauls-mbp.internal.zauberlabs.com>
2014-06-17 17:39:12 -0300

    Removing story model

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,16 +3,11 @@ class StoriesController < ApplicationController
   after_filter :static_content
 
   def index
-    @stories = Story.all
+
   end
 
   def show
-    @story = Story.find(params[:id])
-    @related_stories = Story.where("category = ? AND id != ?", @story.category, @story.id).sample(3)
-    related_stories_count = @related_stories.count
-    if related_stories_count < 3
-      @related_stories.concat Story.where("category != ?", @story.category).sample(3 - related_stories_count)
-    end
+
   end
 
 end
