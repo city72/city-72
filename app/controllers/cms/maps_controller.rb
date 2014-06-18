e51dcd2a9a183031453a036e1188811d1d068585
Paul Ballesty <paulballesty@gmail.com>
2014-06-18 15:33:41 -0300

    Emergency data form

--- app/controllers/cms/maps_controller.rb
+++ app/controllers/cms/maps_controller.rb
@@ -1,9 +1,18 @@
 class Cms::MapsController < BackOfficeController
 
-  def show
-  end
+  before_filter :set_emergency_data
+
+  empty_methods :show
 
   def update
+    @emergency_data.attributes = params[:emergency_data]
+    @emergency_data.save
+    render :show
+  end
+
+  private
+  def set_emergency_data
+    @emergency_data = EmergencyData.first
   end
 
 end
