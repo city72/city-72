d91a26a5b47615204a7d3c15d24996d3b6aea0f5
damian <damian@zauberlabs.com>
2014-06-19 19:34:43 -0300

    uploaders sizes fixed

--- app/uploaders/resident_image_uploader.rb
+++ app/uploaders/resident_image_uploader.rb
@@ -1,14 +1,14 @@
 # encoding: utf-8
 
 class ResidentImageUploader < BaseImageUploader
-  process :resize_and_pad => [600, 380]
+  process :resize_and_fill => [1420, 416]
 
   version :common do
-    process :resize_and_pad => [300, 190]
+    process :resize_and_fill => [710, 416]
   end
 
   version :retina do
-    process :resize_and_pad => [600, 380]
+    process :resize_and_fill => [1420, 416]
   end
 
 end
