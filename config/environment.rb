c508678e80c4bf7817923adfb08f6760016e23d6
Paul Ballesty <paulballesty@gmail.com>
2014-06-24 11:12:41 -0300

    Adding encodings in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
 City72::Application.initialize!
