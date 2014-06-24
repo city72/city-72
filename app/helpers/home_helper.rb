5777fd6d74fca436e2d6f37811fbf5fb1e016ec3
Paul Ballesty <paulballesty@gmail.com>
2014-06-24 12:31:17 -0300

    Small detail in twitter handles

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,7 +3,7 @@ module HomeHelper
     if tw_accounts.empty?
       " - "
     else
-      tw_accounts = tw_accounts.map{ |ta| "<a href=\"http://twitter.com/@#{ta.username}\" target=\"_blank\">@#{ta.username}</a>" }
+      tw_accounts = tw_accounts.map{ |ta| "<a href=\"http://twitter.com/@#{ta.username}\" target=\"_blank\">#{ta.username}</a>" }
       ([tw_accounts.first(tw_accounts.size - 1).join(", "), tw_accounts.last(1)] - [""]).join(" and ")
     end
   end
