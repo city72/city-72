7f0f00885079b0e0dedfc454eacb58b6c585c038
damian <damian@zauberlabs.com>
2014-06-25 11:40:27 -0300

    adding abbreviation to title, deleting old files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "City72"
+    base_title = "#{@city.abbreviation}72"
     if page_title.empty?
       base_title
     else
