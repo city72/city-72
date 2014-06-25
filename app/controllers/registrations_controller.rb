53372fb0a3d8a0b0de331082e6ee7bd41b937aa9
Paul Ballesty <paulballesty@gmail.com>
2014-06-25 16:40:11 -0300

    Showing errors in each form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,12 @@ class RegistrationsController < Devise::RegistrationsController
     render nothing: true, status: 503
   end
 
+  def update
+    @form_name = params[:admin_user][:form_name]
+    params[:admin_user] = params[:admin_user].except(:form_name)
+    super
+  end
+
   private
   def after_update_path_for(resource)
     session[:previous_url] || cms_root_path
