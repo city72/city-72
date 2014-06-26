ca84748448deef5c533314c45ca8e3132534a055
Paul Ballesty <paulballesty@gmail.com>
2014-06-26 17:01:38 -0300

    Quick fix in city serializer (forgot to remove image credits)

--- app/serializers/city_serializer.rb
+++ app/serializers/city_serializer.rb
@@ -1,7 +1,6 @@
 class CitySerializer < ActiveModel::Serializer
-  attributes :id, :name, :abbreviation, :moniker, :possessive, :color_cd, :image_credit, 
-    :resident_image_credit, :brought_by, :agency_url, :contact_email, :statement, :image_url, 
-    :resident_image_url
+  attributes :id, :name, :abbreviation, :moniker, :possessive, :color_cd, :brought_by, 
+    :agency_url, :contact_email, :statement, :image_url, :resident_image_url
 
     has_many :affiliates
 end
