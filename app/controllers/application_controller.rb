29752d467d0e8222fd48f3166a744f2cf421f461
Arena <arena@visualarena.com>
2015-06-23 22:04:28 -0700

    added yml files
    Tagolog (t)l, Vietnamese (vi), and Chinese (zh)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,12 @@ class ApplicationController < ActionController::Base
   end
  
   def set_locale
-    I18n.locale = 'en' #params[:locale] || I18n.default_locale
+    #I18n.locale = 'en' #params[:locale] || I18n.default_locale
+    #I18n.locale = 'es'
+    #I18n.locale = 'vi'
+    #I18n.locale = 'zh'
+    I18n.locale = 'tl'
+
   end
 
   protected
